1daac9314fb465880c62a40088f665337878637a
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-11-17 12:09:58 +0600

    chore: add default pages

---
+++ src/app/location/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+        <ol>
+          <li>
+            Get started by editing <code>src/app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+    </div>
+  );
+}
