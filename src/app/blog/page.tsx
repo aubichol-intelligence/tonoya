2d2b29609f721439bb70e4fe1e40ff8c6293709a
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-11-19 18:09:41 +0600

    chore: update dummy blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,44 @@
 import Image from "next/image";
 
-export default function Home() {
+/*
+export async function generateStaticParams() {
+  let posts = await fetch('https://api.vercel.app/blog', {
+    cache: 'force-cache',
+  }).then((res) => res.json())
+ 
+  return posts.map((post: Post) => ({
+    id: post.id,
+  }))
+}
+ 
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  //let post = await getPost(params.id)
+ 
+  return {
+    title: post.title,
+  }
+}
+*/
+
+import BlogList from '../../../components/BlogList';
+import blogPosts from '../../../components/data/blogs.json';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Blog</h1>
+      <BlogList posts={blogPosts} />
+    </div>
+  );
+};
+
+export default Home
+/*
   return (
     <div >
         <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
           <li>Save and see your changes instantly.</li>
         </ol>
     </div>
   );
-}
+*/
