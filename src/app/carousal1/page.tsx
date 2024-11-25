29e9774f6a4a7ec85c71dda11ef5fbc6ff8c18d0
Avik2293 <aviksarker22@gmail.com>
2024-11-25 22:43:31 +0600

    fix: some build error

--- src/app/carousal1/page.tsx
+++ src/app/carousal1/page.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const svgImages = [
-    { id: 1, src: "https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 1" },
-    { id: 2, src: "https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 2" },
-    { id: 3, src: "https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 3" },
-    { id: 4, src: "/https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 4" },
-    { id: 5, src: "/https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 5" },
-  ];
+  { id: 1, src: "https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 1" },
+  { id: 2, src: "https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 2" },
+  { id: 3, src: "https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 3" },
+  { id: 4, src: "https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 4" },
+  { id: 5, src: "https://i.ibb.co.com/qJBw5kK/DIU.jpg", alt: "SVG 5" },
+];
 
 const Carousel = () => {
-    const duplicatedImages = [...svgImages, ...svgImages];
-    return (
-        <div className={styles.carouselWrapper}>
-          <div className={styles.carousel}>
-            {duplicatedImages.map((image, index) => (
-              <div className={styles.carouselItem} key={`${image.id}-${index}`}>
-                <img src={image.src} alt={image.alt} className={styles.image} />
-              </div>
-            ))}
+  const duplicatedImages = [...svgImages, ...svgImages];
+  return (
+    <div className={styles.carouselWrapper}>
+      <div className={styles.carousel}>
+        {duplicatedImages.map((image, index) => (
+          <div className={styles.carouselItem} key={`${image.id}-${index}`}>
+            <Image src={image.src} alt={image.alt} className={styles.image} />
           </div>
-        </div>
-      );
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
