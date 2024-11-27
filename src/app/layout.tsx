3a1195da08986b8c49a096d5599e49cc0f6117bf
Shajia Annoor <shajia.annoor@gmail.com>
2024-11-27 08:18:15 +0600

    chore: add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import 'leaflet/dist/leaflet.css';
 import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
+import Footer from "./footer1/page";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -34,6 +35,7 @@ export default function RootLayout({
 				<AuthProvider>
 					{children}
 				</AuthProvider>
+				<Footer />
 			</body>
 		</html>
 	);
