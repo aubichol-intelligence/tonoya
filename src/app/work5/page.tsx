ce29c0d6cb08747e37fc6e627d110e517ae74e52
Shajia Annoor <shajia.annoor@gmail.com>
2024-11-27 12:44:53 +0600

    chore: update work related write up

--- src/app/work5/page.tsx
+++ src/app/work5/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./page.module.css";
 
 const ThreeCards = () => {
-  
+  /*
   const cards1 = [
     {
       id: 1,
@@ -23,27 +23,31 @@ const ThreeCards = () => {
         "We craft seamless, innovative websites that captivate and deliver functionality in every click.",
     },
   ];
-  
- /*
-  const cards = [
+  */
+ 
+  const cards1 = [
     { 
+      id: 1,
       title: "energy efficient", 
       description: "An energy-efficient vending machine is designed to minimize power consumption while providing convenience. Equipped with advanced insulation, LED lighting, and energy-saving compressors, it maintains optimal cooling or heating with minimal waste. Smart sensors detect user activity and adjust power usage accordingly, reducing energy when the machine is idle. These machines often incorporate renewable energy sources, such as solar panels, to further lower their environmental impact. By combining technology and sustainability, energy-efficient vending machines offer a cost-effective and eco-friendly solution for modern snack and beverage needs.." 
     },
     { 
+      id: 2,
       title: "energy efficient", 
       description: "An energy-efficient vending machine is designed to minimize power consumption while providing convenience. Equipped with advanced insulation, LED lighting, and energy-saving compressors, it maintains optimal cooling or heating with minimal waste. Smart sensors detect user activity and adjust power usage accordingly, reducing energy when the machine is idle. These machines often incorporate renewable energy sources, such as solar panels, to further lower their environmental impact. By combining technology and sustainability, energy-efficient vending machines offer a cost-effective and eco-friendly solution for modern snack and beverage needs.." 
     },
     { 
+      id: 3,
       title: "energy efficient", 
       description: "An energy-efficient vending machine is designed to minimize power consumption while providing convenience. Equipped with advanced insulation, LED lighting, and energy-saving compressors, it maintains optimal cooling or heating with minimal waste. Smart sensors detect user activity and adjust power usage accordingly, reducing energy when the machine is idle. These machines often incorporate renewable energy sources, such as solar panels, to further lower their environmental impact. By combining technology and sustainability, energy-efficient vending machines offer a cost-effective and eco-friendly solution for modern snack and beverage needs.." 
     },
     { 
+      id: 4,
       title: "energy efficient", 
       description: "An energy-efficient vending machine is designed to minimize power consumption while providing convenience. Equipped with advanced insulation, LED lighting, and energy-saving compressors, it maintains optimal cooling or heating with minimal waste. Smart sensors detect user activity and adjust power usage accordingly, reducing energy when the machine is idle. These machines often incorporate renewable energy sources, such as solar panels, to further lower their environmental impact. By combining technology and sustainability, energy-efficient vending machines offer a cost-effective and eco-friendly solution for modern snack and beverage needs.." 
     },
   ];
-*/
+
   return (
     <section className={styles.cardSection}>
       <div className={styles.cardsContainer}>
