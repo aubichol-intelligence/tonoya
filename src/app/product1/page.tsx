e6d006080e45e3f25cee4020c5bd0a2ae6272857
Shajia Annoor <shajia.annoor@gmail.com>
2024-12-04 22:57:45 +0600

    chore: comment pages

--- src/app/product1/page.tsx
+++ src/app/product1/page.tsx
@@ -1,21 +1,24 @@
 "use client";
-import React, { useState } from 'react';
-import Navbar from '../../components/Product/Navbar/index';
-import Sidebar from '../../components/Product/Sidebar/index';
-import ProductView from '../../components/Product/ProductView/index';
+
+import React from 'react';
+//import Navbar from '../../components/Product/Navbar/index';
+//import Sidebar from '../../components/Product/Sidebar/index';
+//import ProductView from '../../components/Product/ProductView/index';
 import styles from './page.module.css';
 
 const Home = () => {
-  const [menuIndex, setMenuIndex] = useState(null);
-  const [productIndex, setProductIndex] = useState(null);
+//  const [menuIndex, setMenuIndex] = useState(null);
+//  const [productIndex, setProductIndex] = useState(null);
 
   return (
     <div className={styles.container}>
+      {/*
       <Navbar onMenuClick={setMenuIndex} />
       <div className={styles.main}>
-        <Sidebar menuIndex={menuIndex} onItemClick={setProductIndex} />
+        <Sidebar  />
         <ProductView productIndex={productIndex} />
       </div>
+  */}
     </div>
   );
 };
