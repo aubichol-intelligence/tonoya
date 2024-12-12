0adfaab27e4121b922ce08ad54a7231b73571c1c
Tarif Ezaz <tarif.ezaz@gmail.com>
2024-12-12 11:04:29 +0600

    chore: fix centering issue of marker

--- src/components/map/page.jsx
+++ src/components/map/page.jsx
@@ -1,29 +1,29 @@
 'use client';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-//import markerIconPng from "leaflet/dist/images/marker-icon.png";
-//import {Icon} from 'leaflet'
-
 import L from 'leaflet';
-//import { Icon } from 'leaflet';
-
-//const Map = dynamic(() => import('../../components/Map'), { ssr: false }); // Disable SSR for Leaflet
 
 export default function Map() {
-  const icon = L.icon({ iconUrl: '/logos/marker-icon.png' });
+  // Define custom icon with proper alignment
+  const icon = L.icon({
+    iconUrl: '/logos/marker-icon.png', // Path to your custom marker icon
+    iconSize: [25, 41], // Size of the icon [width, height]
+    iconAnchor: [12, 41], // Anchor point for the icon (aligned to the bottom-center)
+  });
+
+  const position = [23.7438111, 90.3665748]; // Latitude and longitude of the marker
+
   return (
     <main>
-      {/*<h1>Welcome to the Map App</h1>*/}
       <MapContainer
-        center={[23.7438111, 90.3665748]} // Initial map center (latitude, longitude)
+        center={position} // Center the map on the marker
         zoom={13}
-        key="map-container"
-        style={{ height: '40vh', width: '100vw'}} // Full-page map
+        style={{ height: '40vh', width: '100vw' }} // Full-page map
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[23.7438111, 90.3665748]} icon={icon}>
+        <Marker position={position} icon={icon} draggable={false}>
           <Popup>
             <b>Tonoya</b>
             <br />270, Sultanganj Road, Rayer Bazar, Dhanmondi, Dhaka
